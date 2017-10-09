266f50b3c5381547491d263ae3e5df5492798dbd
v-nannan <fjdgk>
2017-10-09 18:52:25 +0530

    "changes added"

--- Isola Game/Player.cs
+++ Isola Game/Player.cs
@@ -6,21 +6,21 @@ using System.Threading.Tasks;
 
 namespace Isola_Game
 {
-    class Player
-    {
-        private List<int> SquarePosition
-        {
-            get; set;
-        }
-        public Player(string input)
-        {
-            this.Initialize(input);
-        }
-        public void Initialize(string inputstring)
-        {
-            SquarePosition = inputstring.Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
-        .Select(item => int.Parse(item))
-        .ToList();
-        }
-    }
+	class Player
+	{
+		private List<int> SquarePosition
+		{
+			get; set;
+		}
+		public Player(string input)
+		{
+			this.Initialize(input);
+		}
+		public void Initialize(string inputstring)
+		{
+			SquarePosition = inputstring.Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+		.Select(item => int.Parse(item))
+		.ToList();
+		}
+	}
 }
